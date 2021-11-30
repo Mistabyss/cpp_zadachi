5099aa178ee964d22bfd82fe505a6756d9dd7a49
Dima Sundukov <90245433+Mistabyss@users.noreply.github.com>
2021-11-30 12:00:11 +0500

    Add files via upload

---
+++ zadanie 2.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, pr;
+    pr = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        pr = pr * i;
+    }
+    cout << pr;
+    return 0;
+}
